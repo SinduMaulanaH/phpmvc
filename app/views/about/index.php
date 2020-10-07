a13c9c20cbb81fc4e1836eaaefa82812ad344df0
Sindu Maulana <sindu.maulana@gmail.com>
2020-10-07 20:29:44 +0700

    menambahkan bootstrap, mengubah home dan about dgn menambhakan bootstrap, membuat constants.php dan mengubah init.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-    <h1>About Me</h1>
-    <p>Halo, Nama saya <?= $data['nama']; ?>. Usia saya <?= $data['umur']; ?> tahun. Pekerjaan saya <?= $data['pekerjaan']; ?>. </p>
+    <div class="container">
+        <h1 class="mt-4">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/lisa.jpg" alt="Sindu" width="200" class="rounded-circle shadow">
+        <p>Halo, Nama saya <?= $data['nama']; ?>. Usia saya <?= $data['umur']; ?> tahun. Pekerjaan saya <?= $data['pekerjaan']; ?>. </p>
+    </div>
